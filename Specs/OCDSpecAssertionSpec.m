8228164d58679de74427ee9ffdf23554b0ffa571
Eric Smith <eric@8thlight.com>
2011-04-22 15:39:52 -0500

    Initial assertion spec.

---
+++ Specs/OCDSpecAssertionSpec.m
@@ -0,0 +1,10 @@
+#import "OCDSpec/OCDSpec.h"
+
+CONTEXT(OCDSpecAssertions)
+{
+  describe(@"Assertions", 
+           it(@"has the basic expectation",
+              ^{
+                }),
+           nil);
+}
