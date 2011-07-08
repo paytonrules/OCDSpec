67ab9a1fb3cab792ae8da6f7f4163ae4e871ecd8
Eric Meyer <emeyer@8thlight.com>
2011-07-08 15:03:45 -0500

    expectFalse added

--- OCDSpec/OCDSpecExpectation.h
+++ OCDSpec/OCDSpecExpectation.h
@@ -12,8 +12,10 @@
 -(void) toBeEqualTo:(id) expectedObject;
 -(void) toBe:(id) expectedObject;
 -(void) toBeTrue;
+-(void) toBeFalse;
 
 @end
 
 #define expect(obj)       [[[OCDSpecExpectation alloc] initWithObject:obj inFile:[NSString stringWithUTF8String:__FILE__] atLineNumber:__LINE__] autorelease]
 #define expectTruth(obj)  [[[[OCDSpecExpectation alloc] initWithObject:[NSNumber numberWithBool:obj] inFile:[NSString stringWithUTF8String:__FILE__] atLineNumber:__LINE__] autorelease] toBeTrue]
+#define expectFalse(obj)  [[[[OCDSpecExpectation alloc] initWithObject:[NSNumber numberWithBool:obj] inFile:[NSString stringWithUTF8String:__FILE__] atLineNumber:__LINE__] autorelease] toBeFalse]
