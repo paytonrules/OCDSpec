8431f58cbbe09c9a86292e3fe5b7af800fabdc81
Eric Smith <eric@8thlight.com>
2012-05-25 16:38:48 -0500

    Fit custom matcher example in the way I want it

--- Specs/OCDCustomMatcherSpec.m
+++ Specs/OCDCustomMatcherSpec.m
@@ -3,22 +3,22 @@
 
 CONTEXT(OCDSpecExpectation_CustomMatcherSpec)
 {
-    describe(@"toEqualABC",
-             it(@"fails when the object is not ABC",
-                ^{
-                    @try
-                    {
-                        [expect(@"123") toEqualABC];
-                        FAIL(@"Code did not throw a failure exception");
-                    }
-                    @catch (NSException *exception)
-                    {
-                        [expect([exception reason]) toBeEqualTo: @"Expected ABC, but got 123"];
-                    }
-                }),
-             it(@"passes when the object is ABC",
-                ^{
-                    [expect(@"ABC") toEqualABC];
-                }),
-             nil);
+  describe(@"toEqualABC",
+          it(@"fails when the object is not ABC", ^{
+            @try
+            {
+              [expect(@"123") toEqualABC];
+              FAIL(@"Code did not throw a failure exception");
+            }
+            @catch (NSException *exception)
+            {
+              [expect([exception reason]) toBeEqualTo:@"Expected ABC, but got 123"];
+            }
+          }),
+
+          it(@"passes when the object is ABC", ^{
+            [expect(@"ABC") toEqualABC];
+          }),
+
+          nil);
 }
